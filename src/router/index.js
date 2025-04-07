f4a6df050cf2f8a5ce62f19ae8b65a4b078571d8
aaronjayesabuero@gmail.com <aaronjayesabuero@gmail.com>
2025-04-08 01:01:22 +0800

    add a log in form

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SignupView from '@/views/SignupView.vue'
 import LoginView from '@/views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/signup',
+      name: 'signup',
+      component: SignupView,
+    },
+    {
+      path: '/login',
+      name: 'login',
       component: LoginView,
     },
 
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -18,36 +18,33 @@ const theme = ref('green')
         <v-container class="fill-height d-flex align-center justify-center">
           <v-row class="justify-center">
             <v-col cols="12" md="6">
-              
               <v-card class="mx-auto my-8" max-width="500">
                 <template v-slot:title>
-                  <h2 class="text-center" style="color: black;">Log in</h2>
+                  <h2 class="text-center" style="color:black;">Sign Up</h2>
                 </template>
 
                 <div class="d-flex justify-center gap-4 mt-2">
-                  <RouterLink to="/signup">
-                    <v-btn class="mx-1" color="#D9D9D9"><h3>Sign up</h3></v-btn>
-                  </RouterLink>
-                  <v-btn class="mx-1" color="#3CCC32"><h3>Log in</h3></v-btn>
+                  <v-btn class="mx-1" color="#3CCC32"><h3>Sign up</h3></v-btn>
+
+                  <router-link to="/login">
+                    <v-btn class="mx-1" color="#D9D9D9"><h3>Log in</h3></v-btn>
+                  </router-link>
                 </div>
 
                 <v-card-text class="pt-4">
                   <v-form fast-fail @submit.prevent>
-                    <v-text-field
-                      label="Email"
-                      prepend-inner-icon="mdi-email"
-                    />
+                    <v-text-field label="Email" prepend-inner-icon="mdi-email" />
+
+                    <v-text-field label="Password" type="password" prepend-inner-icon="mdi-lock" />
 
                     <v-text-field
-                      label="Password"
+                      label="Confirm password"
                       type="password"
-                      prepend-inner-icon="mdi-lock"
+                      prepend-inner-icon="mdi-lock-check"
                     />
-
-                    <h4 class="mb-7">Forget password?</h4>
 
                     <v-btn class="mt-2" type="submit" block color="#3CCC32">
-                      <h3>Log in</h3>
+                      <h3>Sign up</h3>
                     </v-btn>
                   </v-form>
                 </v-card-text>
